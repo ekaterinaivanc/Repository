817b1e4c1fa74ad8e9fe89957d35b933b0cd4d29
ekaterinaivanc <kataivancova7@gmail.com>
2025-11-10 12:56:46 +0300

    Sixth commit

--- main.cpp
+++ main.cpp
@@ -5,27 +5,36 @@ struct IntArray {
   int get(size_t id) const;
   size_t size() const;
   int last() const;
+  IntArray(int i);
+  ~IntArray();
 };
 
 int main()
 {  
   int next = 0;
   std::cin >> next;
-  IntArray a;
-  a.add(next);
-  while (std::cin >> next)
-  {
-    a.add(next);
+  try {
+    IntArray a(next); //пользовательский конструктор
+    //IntArray a; //конструктор по умолчанию
+    //a.add(next);
+    while (std::cin >> next)
+    {
+        a.add(next);
+    }
+    if (std::cin.fail() && !std::cin.eof())
+    {
+        return 1;
+    }
+    size_t count = 1;
+    for (size_t i = 0; i < a.size() - 1; ++i)
+    {
+        int d = a.get(i);
+        count += !(d % a.last());
+    }
+    std::cout << count << "\n";
   }
-  if (std::cin.fail())
-  {
-    return 1;
-  }
-  size_t count = 1;
-  for (size_t i = 0; i < a.size() - 1; ++i)
-  {
-    int d = a.get(i);
-    count += !(d % a.last());
+  catch (const std::bad_alloc) {
+    return 2;
   }
 }
 
