a429b855801b46f4388955afb78f3e5da332521d
ekaterinaivanc <kataivancova7@gmail.com>
2025-11-10 13:33:12 +0300

    Ninth commit

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 
 struct IntArray {
   void add(int i);
-  int get(size_t id) const;
-  size_t size() const;
-  int last() const;
+  int get(size_t id) const noexcept;
+  size_t size() const noexcept;
+  int last() const noexcept;
   IntArray(int i);
   ~IntArray();
   int * a;
   size_t k;
+  int at(size_t id()) const;
 };
 
 int main()
@@ -16,9 +17,7 @@ int main()
   int next = 0;
   std::cin >> next;
   try {
-    IntArray a(next); //пользовательский конструктор
-    //IntArray a; //конструктор по умолчанию
-    //a.add(next);
+    IntArray a(next);
     while (std::cin >> next)
     {
       a.add(next);
@@ -48,5 +47,32 @@ IntArray::IntArray(int i) :
   a(new int[1]),
   k(1)
 {
-  a[0] = i;
+  * a = i;
+}
+
+int IntArray::get(size_t id) const noexcept
+{
+  return a[id];
+}
+
+size_t IntArray::size() const noexcept
+{
+  return k;
+}
+
+int IntArray::last() const noexcept
+{
+  return get(size() - 1);
+}
+
+void IntArray::add(int i)
+{
+  int * tmp = new int[size() + 1];
+  for (size_t i = 0; i < size(); ++i)
+  {
+    tmp[i] = get(i);
+  }
+  delete[] a;
+  a = tmp;
+  ++k;
 }
